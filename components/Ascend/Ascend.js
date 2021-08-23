eb4255881e80db5e4cf5474992ed4ea445d89d38
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2021-08-23 14:28:52 -0500

    chore: different material view v3

--- components/Ascend/Ascend.js
+++ components/Ascend/Ascend.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 
 import Card from '../Card/Card'
 
@@ -6,6 +6,7 @@ import Locals from '../../data/local'
 import Boss from '../../data/boss'
 import Gems from '../../data/gems'
 import Commons from '../../data/common'
+import AddIcon from '@material-ui/icons/Add'
 
 const Ascend = props => {
 	let local = props.ascend.local
@@ -13,43 +14,56 @@ const Ascend = props => {
 	let gem = props.ascend.gem
 	let common = props.ascend.common
 
+	const [VIEW, setView] = useState(false)
+	const toggleState = () => setView(value => !value)
+
+	let cardClass = ['ascendCards']
+
+	if (VIEW) {
+		cardClass.push('on')
+	}
+
 	return (
-		<div className="ascendCards">
-			{/* <h3>Materials</h3> */}
-			<Card
-				type="local"
-				id={local}
-				name={Locals[local].name}
-				rarity={1}
-				size="small"
-				tooltip={true}
-			/>
-			{boss && (
+		<div className={cardClass.join(' ')}>
+			<h3 onClick={toggleState}>
+				Materials <AddIcon fontSize="small" />
+			</h3>
+			<div className="materials">
 				<Card
-					type="boss"
-					id={boss}
-					name={Boss[boss].name}
+					type="local"
+					id={local}
+					name={Locals[local].name}
+					rarity={1}
+					size="small"
+					tooltip={true}
+				/>
+				{boss && (
+					<Card
+						type="boss"
+						id={boss}
+						name={Boss[boss].name}
+						rarity={3}
+						size="small"
+						tooltip={true}
+					/>
+				)}
+				<Card
+					type="gem"
+					id={gem}
+					name={Gems[gem][3]}
 					rarity={3}
 					size="small"
 					tooltip={true}
 				/>
-			)}
-			<Card
-				type="gem"
-				id={gem}
-				name={Gems[gem][3]}
-				rarity={3}
-				size="small"
-				tooltip={true}
-			/>
-			<Card
-				type="common"
-				id={common}
-				name={Commons[common][3]}
-				rarity={3}
-				size="small"
-				tooltip={true}
-			/>
+				<Card
+					type="common"
+					id={common}
+					name={Commons[common][3]}
+					rarity={3}
+					size="small"
+					tooltip={true}
+				/>
+			</div>
 		</div>
 	)
 }
